60b4ec32bd0f04e4e39855ead59d69b881f62eb2
Adam S <adam.sarli@gmail.com>
2019-03-29 18:04:59 +0000

    Added Phase 1 menu

--- lib/marvel/CLI.rb
+++ lib/marvel/CLI.rb
@@ -51,7 +51,7 @@ class CLI
       input = gets.strip.downcase 
       case input 
       when "1" || "phase 1"
-        puts "More info on Phase 1"
+        phase1 
       when "2" || "phase 2"
         puts "More info on Phase 2"
       when "3" || "phase 3"
@@ -63,4 +63,18 @@ class CLI
       end 
     end 
   end 
+  
+  def phase1 
+    puts ""
+    puts " ------------------ Phase 1 -----------------"
+    puts "1. Iron Man - May 2, 2008"
+    puts "2. The Incredible Hulk - June 13, 2008"
+    puts "3. Iron Man 2 - May 7, 2010"
+    puts "4. Thor - May 6, 2011"
+    puts "5. Captain America: The First Avenger - July 22, 2011"
+    puts "6. Marvel's The Avengers - May 4, 2012"
+    puts "Select a film number:"
+    input = gets.strip.downcase 
+  end 
+  
 end 
