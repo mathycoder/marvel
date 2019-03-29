1a15da2c01c9788700d6731a6ab05c66412e4e84
Adam S <adam.sarli@gmail.com>
2019-03-29 18:35:23 +0000

    installed some scraping Gems

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in marvel.gemspec
+gem 'pry'
+gem 'nokogiri', '1.6.6.2'
--- lib/marvel.rb
+++ lib/marvel.rb
@@ -1,5 +1,8 @@
 #This is the gem's environment file 
 
+require 'open-uri'
+require 'nokogiri'
+
 require_relative "marvel/version"
 require_relative "marvel/CLI"
 
