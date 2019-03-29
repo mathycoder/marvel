09741e6e5b9de6a178f127922ec63f449e57ee1c
Adam S <adam.sarli@gmail.com>
2019-03-29 18:43:07 +0000

    error fix

--- lib/marvel.rb
+++ lib/marvel.rb
@@ -1,5 +1,7 @@
 #This is the gem's environment file 
 
+
+
 require 'open-uri'
 require 'nokogiri'
 
--- lib/marvel/scraper.rb
+++ lib/marvel/scraper.rb
@@ -1,7 +1,7 @@
 # Run this in IRB to test things out 
 # require_relative './lib/marvel.rb'
 
- Scraper 
+class Scraper 
   attr_accessor :path 
   
   def initialize(path="https://en.wikipedia.org/wiki/List_of_Marvel_Cinematic_Universe_films")
