493dd75eec38b731c29facc9fb2dc066903c2eb7
Adam S <adam.sarli@gmail.com>
2019-03-29 17:54:12 +0000

    Worked on CLI menu

--- lib/marvel/CLI.rb
+++ lib/marvel/CLI.rb
@@ -1,9 +1,34 @@
 class CLI 
   
   def run 
+    logo 
     welcome
     phase_menu 
-    input = user_input
+  end 
+  
+  def logo 
+    logo_string = %q( 
+      ──────────────▐█████───────
+      ──────▄▄████████████▄──────
+      ────▄██▀▀────▐███▐████▄────
+      ──▄██▀───────███▌▐██─▀██▄──
+      ─▐██────────▐███─▐██───██▌─
+      ─██▌────────███▌─▐██───▐██─
+      ▐██────────▐███──▐██────██▌
+      ██▌────────███▌──▐██────▐██
+      ██▌───────▐███───▐██────▐██
+      ██▌───────███▌──▄─▀█────▐██
+      ██▌──────▐████████▄─────▐██
+      ██▌──────█████████▀─────▐██
+      ▐██─────▐██▌────▀─▄█────██▌
+      ─██▌────███─────▄███───▐██─
+      ─▐██▄──▐██▌───────────▄██▌─
+      ──▀███─███─────────▄▄███▀──
+      ──────▐██▌─▀█████████▀▀────
+      ──────███──────────────────
+    ) 
+    puts logo_string 
+
   end 
   
   def welcome 
@@ -11,16 +36,31 @@ class CLI
     puts "" 
   end 
   
+  def goodbye 
+    puts "Back to reality!"
+  end 
+  
   def phase_menu 
-    puts "1. Phase 1"
-    puts "2. Phase 2"
-    puts "3. Phase 3"
-    puts ""
-    puts "Select a Phase:"
+    input = nil 
+    while input != "exit"
+      puts "1. Phase 1"
+      puts "2. Phase 2"
+      puts "3. Phase 3"
+      puts ""
+      puts "Select a Phase or type 'exit':"
+      input = gets.strip.downcase 
+      case input 
+      when "1" || "phase 1"
+        puts "More info on Phase 1"
+      when "2" || "phase 2"
+        puts "More info on Phase 2"
+      when "3" || "phase 3"
+        puts "More info on Phase 3"
+      when "exit"
+        goodbye 
+      else 
+        puts "Please enter a valid Phase"
+      end 
+    end 
   end 
-  
-  def user_input 
-    gets.strip 
-  end 
-  
 end 
